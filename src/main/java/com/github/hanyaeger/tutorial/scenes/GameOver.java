9b1a4b2e150822ec5a504e3224acd166ea7d0cdf
Jengo Wiegerinck <jengowiegerinckcontact@gmail.com>
2025-03-31 14:51:01 +0200

    feat: setup project structure

---
+++ src/main/java/com/github/hanyaeger/tutorial/scenes/GameOver.java
@@ -0,0 +1,15 @@
+package com.github.hanyaeger.tutorial.scenes;
+
+import com.github.hanyaeger.api.scenes.StaticScene;
+
+public class GameOver extends StaticScene {
+    @Override
+    public void setupScene() {
+
+    }
+
+    @Override
+    public void setupEntities() {
+
+    }
+}
