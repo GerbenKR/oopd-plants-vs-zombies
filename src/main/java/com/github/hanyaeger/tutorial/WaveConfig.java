b42b699f2f3b13568f1437600e675c0a6238d9d2
GerbenKr <gerbiebor@outlook.com>
2025-04-03 12:20:04 +0200

    fix: added onCollision to zombies & added waiting time for waves

--- src/main/java/com/github/hanyaeger/tutorial/WaveConfig.java
+++ src/main/java/com/github/hanyaeger/tutorial/WaveConfig.java
@@ -1,16 +1,18 @@
 package com.github.hanyaeger.tutorial;
 
 public class WaveConfig {
+    private WaveType waveType;
     private int durationMs;
     private int spawnRateMs;
 
-    // @TODO: Misschien geen gebruik maken van wave types, maar altijd de laatste wave als final wave defineren?
     public enum WaveType {
+        WAITING,
         WAVE,
         FINAL_WAVE,
     }
 
-    public WaveConfig(WaveType type, int durationMs, int spawnRateMs) {
+    public WaveConfig(WaveType waveType, int durationMs, int spawnRateMs) {
+        this.waveType = waveType;
         this.durationMs = durationMs;
         this.spawnRateMs = spawnRateMs;
     }
@@ -21,5 +23,9 @@ public class WaveConfig {
 
     public int getSpawnRateMs() {
         return spawnRateMs;
+    }
+
+    public WaveType getWaveType() {
+        return waveType;
     }
 }
--- src/main/java/com/github/hanyaeger/tutorial/entities/zombies/BalloonZombie.java
+++ src/main/java/com/github/hanyaeger/tutorial/entities/zombies/BalloonZombie.java
@@ -1,11 +1,29 @@
 package com.github.hanyaeger.tutorial.entities.zombies;
 
 import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.Collider;
+import com.github.hanyaeger.api.entities.Direction;
+import com.github.hanyaeger.tutorial.entities.plants.Plant;
+
+import java.util.List;
 
 public class BalloonZombie extends Zombie {
 
     public BalloonZombie(Coordinate2D location) {
         super(location, "sprites/balloon-zombie.gif", 200, 0.3, 100);
+    }
+
+    @Override
+    public void onCollision(List<Collider> list) {
+        if (this.getHealth() <= 0) {
+            remove();
+        }
+
+        for (Collider collider : list) {
+            if (collider instanceof Plant) {
+                setMotion(0, Direction.LEFT);
+            }
+        }
     }
 
 }
