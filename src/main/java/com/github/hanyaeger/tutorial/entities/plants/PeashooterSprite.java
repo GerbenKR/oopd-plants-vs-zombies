219438a2d3f58b6f86c00bf8d5331f1e47876b00
Jengo Wiegerinck <jengowiegerinckcontact@gmail.com>
2025-04-03 10:58:27 +0200

    fix: resize plants

--- src/main/java/com/github/hanyaeger/tutorial/entities/plants/PeashooterSprite.java
+++ src/main/java/com/github/hanyaeger/tutorial/entities/plants/PeashooterSprite.java
@@ -2,11 +2,12 @@ package com.github.hanyaeger.tutorial.entities.plants;
 
 import com.github.hanyaeger.api.AnchorPoint;
 import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.Size;
 import com.github.hanyaeger.api.entities.impl.SpriteEntity;
 
 public class PeashooterSprite extends SpriteEntity {
     protected PeashooterSprite(String resource, Coordinate2D initialLocation) {
-        super(resource, initialLocation);
+        super(resource, initialLocation, new Size(50, 50));
         setAnchorPoint(AnchorPoint.TOP_LEFT);
     }
 }
